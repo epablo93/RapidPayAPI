897e6f485ac475005ef9246f1bd9a7415d5c2987
Pablo Escobar <Pablo.Escobar@endava.com>
2022-01-29 12:37:04 -0300

    Add project files.

---
+++ Domain/Entities/Balance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Balance
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string CardNumber { get; set; }
+    }
+}
